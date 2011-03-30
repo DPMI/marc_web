3ee986e2bf8c80430c2913838f4b788bca589663
David Sveningsson <ext@sidvind.com>
2011-03-30 08:22:45 +0000

    session_is_registed is deprecated, using  superglobal instead (and fixing useless branch)
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/MArC_web/trunk@1492 8459abcf-8237-4014-9f1e-057f68ddaf13

--- sessionCheck.php
+++ sessionCheck.php
@@ -2,12 +2,6 @@
 session_start();
 // Use $HTTP_SESSION_VARS with PHP 4.0.6 or less
 $noSESSION=0;
-if (!session_is_registered("OK")) {
-//    header("Location: loginDenied.php");
-$SESSION=0;
-} else {
-//    print "OK";
-$SESSION=1;
+$SESSION = isset($_SESSION['OK']) && $_SESSION['OK'] == 'OK';
 
-}
 ?>
