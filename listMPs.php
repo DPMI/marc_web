c5dd4187b108bf630f6c39cdf7db7e5fa505093d
David Sveningsson <ext@sidvind.com>
2011-03-30 11:29:53 +0000

    removing pageStyle from listMP.php, using style.css instead. passing v3c validator
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/MArC_web/trunk@1498 8459abcf-8237-4014-9f1e-057f68ddaf13

--- listMPs.php
+++ listMPs.php
@@ -14,44 +14,53 @@ $mps = MP::selection(array(
 ));
 
 ?>
-<html>
-<?=$pageStyle?>
-
-<table border="0">
-  <tr>
-    <th>Status</th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=id&amp;asc=<?=$ascinv?>">ID</th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=name&amp;asc=<?=$ascinv?>">name</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=ip&amp;asc=<?=$ascinv?>">ip</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=port&amp;asc=<?=$ascinv?>">ip</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=mac&amp;asc=<?=$ascinv?>">mac</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=comment&amp;asc=<?=$ascinv?>">comment</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=time&amp;asc=<?=$ascinv?>">time</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=MAMPid&amp;asc=<?=$ascinv?>">MAMPid</a></th>
-    <th><a href="listMPs.php?SID=<?=$sid?>&amp;order=maxFilters&amp;asc=<?=$ascinv?>">maxFilters</a></th>
-    <th>Authorize MP</th>
-  </tr>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="http://www.bth.se/bth/styles/bth.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="shortcut icon" type="image/x-icon" href="http://www.bth.se/favicon.ico" />
+    <title>MArC :: Measurement points</title>
+  </head>
+  
+  <body class="bthcss">
+    <table border="0">
+      <tr>
+	<th>Status</th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=id&amp;asc=<?=$ascinv?>">ID</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=name&amp;asc=<?=$ascinv?>">name</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=ip&amp;asc=<?=$ascinv?>">ip</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=port&amp;asc=<?=$ascinv?>">ip</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=mac&amp;asc=<?=$ascinv?>">mac</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=comment&amp;asc=<?=$ascinv?>">comment</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=time&amp;asc=<?=$ascinv?>">time</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=MAMPid&amp;asc=<?=$ascinv?>">MAMPid</a></th>
+	<th><a href="listMPs.php?SID=<?=$sid?>&amp;order=maxFilters&amp;asc=<?=$ascinv?>">maxFilters</a></th>
+	<th>Authorize MP</th>
+      </tr>
+	
 <?php foreach ( $mps as $mp ){ ?>
 <?php $color = ($toggle++ % 2 == 0) ? "CCC" : "DDD"; ?>
-  <tr style="background: #<?=$color?>;">
-    <td><?=$mp->status()?></td>
-    <td><?=$mp->id?></td>
-    <td><?=$mp->name?></td>
-    <td><?=$mp->ip?></td>
-    <td><?=$mp->port?></td>
-    <td><?=$mp->mac?></td>
-    <td><?=strlen($mp->comment) > 0 ? $mp->comment : "&nbsp;" ?></td>
-    <td><?=$mp->time?></td>
-    <td><?=$mp->MAMPid?></td>
-    <td><?=$mp->maxFilters?></td>
-    <?php if ( $mp->is_authorized() ){ ?>
-    <td>Authorized</td>
-    <? } else { ?>
-    <td><a href="authMP.php?SID=<?=$sid?>&amp;id=<?=$mp->id?>">Auth</a></td>
-    <? } ?>
-  </tr>
+      <tr style="background: #<?=$color?>;">
+	<td><?=$mp->status()?></td>
+	<td><?=$mp->id?></td>
+	<td><?=$mp->name?></td>
+	<td><?=$mp->ip?></td>
+	<td><?=$mp->port?></td>
+	<td><?=$mp->mac?></td>
+	<td><?=strlen($mp->comment) > 0 ? $mp->comment : "&nbsp;" ?></td>
+	<td><?=$mp->time?></td>
+	<td><?=$mp->MAMPid?></td>
+	<td><?=$mp->maxFilters?></td>
+<?php if ( $mp->is_authorized() ){ ?>
+	<td>Authorized</td>
+<?php } else { ?>
+	<td><a href="authMP.php?SID=<?=$sid?>&amp;id=<?=$mp->id?>">Auth</a></td>
+<?php } ?>
+      </tr>
 <?php } /* foreach $mps */ ?>
-</table>
-
-</body></html>
+    </table>
+      
+  </body>
+</html>
