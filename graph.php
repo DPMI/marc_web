f56686815a0605f3c908c5ff1c80b00703d520d4
David Sveningsson <ext@sidvind.com>
2011-05-07 20:25:23 +0000

    graphs
    git-svn-id: svn://sidvind.com/project/BTH/NPLDP/MArC_web/trunk@1747 8459abcf-8237-4014-9f1e-057f68ddaf13

---
+++ graph.php
@@ -0,0 +1,56 @@
+<?php
+
+require("sessionCheck.php");
+require("config.inc");
+require('model/MP.php');
+
+$mampid = $_GET['mampid'];
+$ci = isset($_GET['CI']) ? $_GET['CI'] : false;
+$span = isset($_GET['span']) ? $_GET['span'] : '24h';
+$mp = MP::from_mampid($mampid);
+/**@todo handle all error conditions */
+
+chdir($rrdbase);
+
+$filebase = "$mampid";
+$title = $mp->name;
+if ( $ci !== false ){
+  $filebase = "{$mampid}_CI$ci";
+  $title = "{$mp->name} CI$ci";
+}
+
+$filename = "{$filebase}_{$span}.png";
+$regen = true;
+$stat = @stat($filename);
+$regen = $stat == false || (time() - $stat['mtime'] > 5*60);
+
+if ( $regen ){
+  $argv = array(
+    "rrdtool", "graph", 
+    "$filename",
+    "-a", "PNG",
+    "--title", $title,
+    "--vertical-label", "pkt/sec",
+    "--start", "end-$span",
+    "DEF:total=$filebase.rrd:total:AVERAGE", "VDEF:total_last=total,TOTAL",
+    "DEF:matched=$filebase.rrd:matched:AVERAGE", "VDEF:matched_last=matched,TOTAL",
+    "LINE1:total#ff0000:Received:",
+    "GPRINT:total_last:%.0lf pkts",
+    "AREA:matched#00ff00:Matched",
+    "GPRINT:matched_last:%.0lf pkts"
+  );
+  $cmd = "'" . implode("' '", $argv) . "'";
+  exec("$cmd 2>&1", $output, $rc);
+  if ( $rc != 0 ){
+    echo "<h1>RRDtool error</h1>\n";
+    echo "<p>Command: \"$cmd\"<br/>Returncode: $rc<p>\n";
+    echo "<p>Output:</p>\n";
+    echo "<pre>" . implode("\n", $output) . "</pre>";
+    exit;
+  }
+}
+
+header ("Content-type: image/png");
+echo file_get_contents($filename);
+
+?>
--- view/mp/view.php
+++ view/mp/view.php
@@ -9,12 +9,12 @@
 </style>
 <table>
   <tr>
-    <th>Status</th>
-    <td><b><?=$mp->status()?></b></td>
+    <th>ID</th>
+    <td><?=$mp->id?></td>
   </tr>
   <tr>
-    <th>ID</th>
-    <td><a href="<?=$index?>/MP/view/<?=$mp->MAMPid?>"><?=$mp->id?></a></td>
+    <th>Status</th>
+    <td><?=$mp->status()?></td>
   </tr>
   <tr>
     <th>IP:Port</th>
@@ -34,4 +34,28 @@
     <th>Max filters</th>
     <td><?=$mp->maxFilters?></td>
   </tr>
+  <tr>
+    <th>CI</th>
+    <td><?=$mp->noCI?> (br0 [PLACEHOLDER])</td>
+  </tr>
+  <tr>
+    <th>Drivers</th>
+    <td>raw, pcap [PLACEHOLDER]</td>
+  </tr>
+  <tr>
+    <th>Versions</th>
+    <td>caputils-0.7.1 libmarc-0.7.0 mp-0.7.0 [placeholder]</td>
+  </tr>
 </table>
+
+<h2>Overview</h2>
+<p>(Graphs are updated at a 5min interval)</p>
+<img src="<?=$root?>graph.php?mampid=<?=$mp->MAMPid?>&amp;span=24h" />
+<img src="<?=$root?>graph.php?mampid=<?=$mp->MAMPid?>&amp;span=60d" />
+
+<h2>Capture Interfaces</h2>
+<?php for ($ci=0; $ci < $mp->noCI; $ci++){ ?>
+<img src="<?=$root?>graph.php?mampid=<?=$mp->MAMPid?>&amp;CI=<?=$ci?>&amp;span=24h" />
+<img src="<?=$root?>graph.php?mampid=<?=$mp->MAMPid?>&amp;CI=<?=$ci?>&amp;span=60d" />
+<br/>
+<?php } ?>
