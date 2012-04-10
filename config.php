9b3e53b67102ed7d556ee8cdca46aa877c4278a3
David Sveningsson <ext@sidvind.com>
2012-04-10 11:54:23 +0200

    fixes lineendings and removes a else-statement.

--- config.php
+++ config.php
@@ -53,7 +53,7 @@ if ( $config_check ){
 			    'current group' => $tmp['name']
 			    );
   }
-  
+
   /* Check that the user apache runs as is a member of the selected group */
   if ( !in_array($usergroup['gid'], posix_getgroups()) ){
     $config_error[] = array(
@@ -70,44 +70,44 @@ if ( count($config_error) > 0 ){
     header("Location: {$root}config_error.php");
     exit;
   }
-} else {
-  
-  /* required for BasicObject */
-  $db = @new mysqli($DB_SERVER, $user, $password, $DATABASE);
-  define('HTML_ACCESS', 1);
-  
-  if ( mysqli_connect_error() ){
-    $config_error[] = array(
-			    "message" => "Unable to connect to MySQL database.",
-			    "error" => mysqli_connect_error(),
-			    );
-  }
-  
-  $max_size=1000000;
-  $Connect = @mysql_connect($DB_SERVER, $user, $password);
-  
-  if ( $Connect ){
-    mysql_select_db($DATABASE,$Connect) or die ("Cant connect to $DATABASE database");
-    
-    $sql_update="SELECT * FROM guiconfig WHERE selected=1";
-    $result=mysql_query($sql_update);
-    if(!$result) {
-      print "MySQL error: " . mysql_error();
-      exit;
-    }
-    if(mysql_num_rows($result)>0) {
-      $row = mysql_fetch_array($result);
-      $pageStyle=$row["pageStyle"];
-      $pageStyleBad=$row["pageStyleBad"];
-      $projectName=$row["projectName"];
-      $selectedID=$row["id"];
-    } else { // PRoblems. Use some default
-      $pageStyle="";
-      $pageStyleBad="";
-      $projectName='MArCd';
-      $selectedID=-1;      
-    }
-  }
+  return;
+}
+
+/* required for BasicObject */
+$db = @new mysqli($DB_SERVER, $user, $password, $DATABASE);
+define('HTML_ACCESS', 1);
+
+if ( mysqli_connect_error() ){
+	$config_error[] = array(
+		"message" => "Unable to connect to MySQL database.",
+		"error" => mysqli_connect_error(),
+		);
+}
+
+$max_size=1000000;
+$Connect = @mysql_connect($DB_SERVER, $user, $password);
+
+if ( $Connect ){
+	mysql_select_db($DATABASE,$Connect) or die ("Cant connect to $DATABASE database");
+
+	$sql_update="SELECT * FROM guiconfig WHERE selected=1";
+	$result=mysql_query($sql_update);
+	if(!$result) {
+		print "MySQL error: " . mysql_error();
+		exit;
+	}
+	if(mysql_num_rows($result)>0) {
+		$row = mysql_fetch_array($result);
+		$pageStyle=$row["pageStyle"];
+		$pageStyleBad=$row["pageStyleBad"];
+		$projectName=$row["projectName"];
+		$selectedID=$row["id"];
+	} else { // PRoblems. Use some default
+		$pageStyle="";
+		$pageStyleBad="";
+		$projectName='MArCd';
+		$selectedID=-1;
+	}
 }
 
 ?>
