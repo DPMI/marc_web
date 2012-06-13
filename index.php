4c25e4120af369d48ea3b14c8cfc5273faedb6e9
David Sveningsson <ext@sidvind.com>
2012-06-13 20:39:27 +0200

    More jquery.

--- index.php
+++ index.php
@@ -61,6 +61,7 @@ if ( $handler == '' ){
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="<?=$root?>style2.css" />
     <link rel="shortcut icon" type="image/x-icon" href="http://www.bth.se/favicon.ico" />
+    <script type="text/javascript" src="<?=$root?>js/jquery-1.7.2.min.js"></script>
     <script type="text/javascript" src="<?=$root?>js/filter.js"></script>
     <title>Network Performance Lab -- MArC</title>
   </head>
--- js/filter.js
+++ js/filter.js
@@ -1,26 +1,9 @@
-function updateIndex() {
-	ci=0;
-	vlan=0;
-	ethtype=0;
-	ethsrc=0;
-	ethdst=0;
-	ipprot=0;
-	ipsrc=0;
-	ipdst=0;
-	srcport=0;
-	dstport=0;
-	if(document.myForm.ci_cb.checked==1){ ci=512;}
-	if(document.myForm.vlan_cb.checked==1) { vlan=256;}
-	if(document.myForm.etht_cb.checked==1) { ethtype=128;}
-	if(document.myForm.eths_cb.checked==1) { ethsrc=64; }
-	if(document.myForm.ethd_cb.checked==1) { ethdst=32; }
-	if(document.myForm.ipp_cb.checked==1) { ipprot=16; }
-	if(document.myForm.ips_cb.checked==1) { ipsrc=8; }
-	if(document.myForm.ipd_cb.checked==1) { ipdst=4; }
-	if(document.myForm.sprt_cb.checked==1) { srcport=2; }
-	if(document.myForm.dprt_cb.checked==1) { dstport=1; }
-	document.myForm.ind.value=ci+vlan+ethtype+ethsrc+ethdst+ipprot+ipsrc+ipdst+srcport+dstport;
-	return;
+function update_index() {
+		var mask = 0;
+		$('.row input:checked').each(function(){
+				mask += 1 << $(this).data('index');
+		});
+		$('#ind').val(mask);
 }
 
 function trim(value){
@@ -29,7 +12,7 @@ function trim(value){
 
 function parse_hex(value){
     if ( value.substr(0,2) == "0x" ){
-	return parseInt(value, 16);
+				return parseInt(value, 16);
     }
     return value;
 }
@@ -49,47 +32,54 @@ function validate_numeric(value){
     return value;
 }
 
+function validate_ethernet(value){
+		if ( value.match(/^[0-9a-fA-F]+$/) == null ){
+				throw "Not valid ethernet address";
+		}
+		return value;
+}
+
 function filter_clear(elem){
-    elem.className = "";
-    elem.title = "";
+		elem.className = "";
+		elem.title = "";
 }
 
 function filter_submit(){
     fields = {
-	'VLAN_TCI': [trim, parse_hex, validate_numeric],
-	'VLAN_TCI_MASK': [trim, parse_hex],
-	'ETH_TYPE_MASK': [trim, parse_hex],
-	'ETH_SRC': [trim, strip_mac, upper],
-	'ETH_DST': [trim, strip_mac, upper],
-	'SRC_PORT': [trim, parse_hex],
-	'SRC_PORT_MASK': [trim, parse_hex],
-	'DST_PORT': [trim, parse_hex],
-	'DST_PORT_MASK': [trim, parse_hex],
-	'CAPLEN': [trim, validate_numeric]
+				'VLAN_TCI': [trim, parse_hex, validate_numeric],
+				'VLAN_TCI_MASK': [trim, parse_hex, validate_numeric],
+				'ETH_TYPE_MASK': [trim, parse_hex, validate_numeric],
+				'ETH_SRC': [trim, strip_mac, upper, validate_ethernet],
+				'ETH_DST': [trim, strip_mac, upper, validate_ethernet],
+				'SRC_PORT': [trim, parse_hex, validate_numeric],
+				'SRC_PORT_MASK': [trim, parse_hex, validate_numeric],
+				'DST_PORT': [trim, parse_hex, validate_numeric],
+				'DST_PORT_MASK': [trim, parse_hex, validate_numeric],
+				'CAPLEN': [trim, validate_numeric]
     };
     var ret = true;
 
     for ( id in fields ){
-	elem = document.getElementById(id);
-	if ( elem == null ){
-	    continue;
-	}
+				elem = document.getElementById(id);
+				if ( elem == null ){
+						continue;
+				}
 
-	try {
-	    pipes = fields[id];
-	    value = elem.value;
-	    for ( func in pipes ){
-		value = pipes[func](value);
-	    }
-	    elem.value = value;
-	} catch ( e ){
-	    elem.className = "invalid";
-	    elem.title = e;
-	    ret = false;
-	}
+				value = elem.value;
+				try {
+						pipes = fields[id];
+						for ( func in pipes ){
+								value = pipes[func](value);
+						}
+						elem.value = value;
+				} catch ( e ){
+						console.log(value + ' failed to validate: ' + e);
+						elem.className = "invalid";
+						elem.title = e;
+						ret = false;
+				}
     }
-    
-    console.log("validated");
+
     return ret;
 }
 
@@ -100,4 +90,21 @@ function filter_cancel(){
      * the form is validated all the time, like when pressing enter. */
     document.myForm.onsubmit = function(){};
     document.myForm.submit();
+}
+
+function filter_init(){
+		$('#ind').attr('readonly', true);
+		$('.row input:checkbox').each(function(){
+				var $row = $(this).parent().parent();
+				var en = !$(this).attr('checked');
+				$row.find('input:text').attr('disabled', en);
+				$row.find('select').attr('disabled', en);
+		});
+		$('.row input:checkbox').click(function(){
+				update_index();
+				var $row = $(this).parent().parent();
+				var en = !$(this).attr('checked');
+				$row.find('input:text').attr('disabled', en);
+				$row.find('select').attr('disabled', en);
+		});
 }
