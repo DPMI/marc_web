3ef1cade1db43ff4acd622de4c59796b0d18a443
David Sveningsson <ext@sidvind.com>
2013-02-03 20:27:28 +0100

    removing extra ?> end tag

--- view/welcome.php
+++ view/welcome.php
@@ -3,7 +3,7 @@
 <?php if ( isset($_SESSION['passwd_warning']) ){ ?>
 <div class="notice" style="border: 1px dashed black; background-position:left center;">
   <p>Your password must be updated. The hashing algorithm changed in 0.7 due to security issues. Your current password will continue to work in this version but future versions may remove this compatability.</p>
-  <p>The password can be updated on the <a href="<?=$index?>/account/self?>">account</a> page.</p>
+  <p>The password can be updated on the <a href="<?=$index?>/account/self">account</a> page.</p>
 </div>
 <?php } ?>
 
