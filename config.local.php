39d234b46d1e2bee0b561b873a10fd756567b709
Patrik Arlos <patrik.arlos@bth.se>
2013-03-16 21:26:03 +0100

    Added README with more instructions on how to get going. Added config.local.php with a starting config.

---
+++ config.local.php
@@ -0,0 +1,18 @@
+<?php
+/* Database configuration */
+$DB_SERVER="localhost";
+$DATABASE='marc2';
+$user='marc';
+$password='konko';
+
+/* Path configuration */
+$localstatedir = "{PREFIX}var/lib/";
+
+$root = '/marc2/';
+$index = $root . 'index.php';
+
+/* General */
+$title = 'Network Performance Labs';
+$subtitle = 'NPLMArC';
+
+?>
