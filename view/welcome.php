014d7cb9c656ba274b9dd4e565675ec5b09f589b
Patrik Arlos <Patrik.Arlos@bth.se>
2013-05-24 19:51:06 +0200

    Added quick info on the front page, as to judge 'status' of the MArC.

--- view/welcome.php
+++ view/welcome.php
@@ -1,4 +1,19 @@
 <?php //  -*- mode:html;  -*- ?>
+<?php 
+require_once('helper/age.php'); 
+require_once('model/MP.php');
+
+$order = isset($_GET['order']) ? $_GET['order'] : 'name';
+$asc = isset($_GET['asc']) ? (int)$_GET['asc'] : 1;
+$ascinv = 1 - $asc;
+$toggle=0;
+
+$mps = MP::selection(array(
+    '@order' => "$order" . ($asc ? '' : ':desc')
+));
+
+?>
+
 <h1>DPMI Measurement Area Control Daemon</h1>
 <?php if ( isset($_SESSION['passwd_warning']) ){ ?>
 <div class="notice" style="border: 1px dashed black; background-position:left center;">
@@ -6,5 +21,38 @@
   <p>The password can be updated on the <a href="<?=$index?>/account/self">account</a> page.</p>
 </div>
 <?php } ?>
-
 <h1>Messages</h1>
+
+
+<h1>Measurement Points</h1>
+<table border="0" cellspacing="0" width="100%" class="list">
+  <tr>
+    <th align="left" valign="bottom">Status</th>
+    <th align="left" valign="bottom"><a href="<?=$index?>/MP?order=MAMPid&amp;asc=<?=$ascinv?>">MAMPid</a></th> 
+    <th align="left" valign="bottom"><a href="<?=$index?>/MP?order=ip&amp;asc=<?=$ascinv?>">ip</a></th>
+    <th align="left" valign="bottom"><a href="<?=$index?>/MP?order=time&amp;asc=<?=$ascinv?>">last heard from</a></th>
+    <th align="left" valign="bottom"><a href="<?=$index?>/MP?order=MAMPid&amp;asc=<?=$ascinv?>">MTU</a></th>
+    <th align="left" valign="bottom"><a href="<?=$index?>/MP?order=time&amp;asc=<?=$ascinv?>">Time Synchronization</a></th>
+    <th align="left" valign="bottom"><a href="<?=$index?>/MP?order=time&amp;asc=<?=$ascinv?>">Software Versions</a></th>
+   </tr>
+
+<?php foreach ( $mps as $mp ){ $toggle = 0; ?>
+  <tr class="<?=($toggle++ % 2 == 0) ? "even" : "odd"?>">
+    <td>
+		<?=$mp->status()?>
+		<?=$mp->ping()?>
+	</td>
+
+    <td><?=$mp->MAMPid?></td>
+    <td><?=$mp->ip?></td>
+    <td><?=age($mp->time)?></td>
+
+    <td><?=$mp->mtu?></td>
+    <td><?=$mp->sync($mp->MAMPid)?></td>
+    <td><?=$mp->version?></td>
+
+
+  </tr>
+<?php } /* foreach $mps */ ?>
+</table>
+
